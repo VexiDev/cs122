228a1d588ec4012ea4030406e6f052e03aaf3942
VexiDev <79677251+VexiDev@users.noreply.github.com>
2024-01-24 17:31:52 -0500

    lab 1 for cs122

---
+++ Labs/Lab01.java
@@ -0,0 +1,11 @@
+package Labs;
+
+public class Lab01 {
+    public static void main(String[] args) {
+        System.out.println("\nHello World!\n");
+        System.out.print("Pepsi\nDr. Pepper\nStarry\n\n");
+        for (int i = 1; i < 6; i++) {
+            System.out.println("*".repeat(i));
+        }
+    }
+   }
